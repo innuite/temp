8d6b07f40adde754c2d16dfa58ce6f5146185231
innuite <innuite@gmail.com>
2020-12-14 09:49:16 +0530

    Add files via upload

--- latencyChecks.sh
+++ latencyChecks.sh
@@ -1,25 +1,29 @@
 date
+gapPrint=3
 echo "============= checking load average - start ================================"
 top -n1 | grep 'load average'
+echo "---------------------------------------"
+sar -q 1 3
 echo "============= checking load average - end =================================="
-
+yes ''| sed ${gapPrint}q
 echo "============= checking tcpdump latency - start ================================"
-CRT_TCP_DUMP_TO=15s
-sudo timeout $CRT_TCP_DUMP_TO tcpdump -ttttt -G 15  &
-CRT_TCP_DUMP_PID=$!
-sleep $CRT_TCP_DUMP_TO
-sudo kill -9 $CRT_TCP_DUMP_PID
+#CRT_TCP_DUMP_TO=15s
+#sudo timeout $CRT_TCP_DUMP_TO tcpdump -ttttt -G 15  &
+#CRT_TCP_DUMP_PID=$!
+#sleep $CRT_TCP_DUMP_TO
+#sudo kill -9 $CRT_TCP_DUMP_PID
 echo "============= checking tcpdump average - end =================================="
-
+yes ''| sed ${gapPrint}q
 echo "============= tracert of integration systems - start ================================"
+# Select the target URL or DNS name to check the trace details.
 traceroute www.google.co.in --mtu --back
 nslookup www.google.co.in
 echo "============= tracert of integration systems - end =================================="
-
+yes ''| sed ${gapPrint}q
 echo "============= details of interrupts - start ================================"
 cat /proc/interrupts
 echo "============= details of interrupts - end ================================"
-
+yes ''| sed ${gapPrint}q
 echo "============= DNS RTT time check - start ================================"
 awk '/nameserver/{print $2}' /etc/resolv.conf | while read line; do
 echo $line
@@ -27,5 +31,23 @@ echo $line
     ping -c 5 $line
 done
 echo "============= DNS RTT time check - end ================================"
-
+yes ''| sed ${gapPrint}q
+echo "============= SOCKET DETAILS check - start ================================"
+sar -n SOCK
+echo "============= SOCKET DETAILS check - end ================================"
+yes ''| sed ${gapPrint}q
+echo "============= Network interface errors check - start ================================"
+sar -n EDEV
+echo "============= Network interface errors check - end ================================"
+yes ''| sed ${gapPrint}q
+echo "============= Network interface statistics check - start ================================"
+sar -n DEV
+echo "============= Network interface statistics check - end ================================"
+yes ''| sed ${gapPrint}q
+echo "============= Asynchronous io requests check - start ================================"
+sysctl fs.aio-max-nr
+sysctl fs.aio-nr
+echo "============= Asynchronous io requests check - end ================================"
+yes ''| sed ${gapPrint}q
+exit 0
 
--- memoryChecks.sh
+++ memoryChecks.sh
@@ -1,24 +1,38 @@
 date
+gapPrint=3
 echo"============= checking memory faults - start ================================"
 # A minor fault means the page is in memory but not allocated to the requesting process or not marked as present in the memory management unit. A major fault means the page in no longer in memory.
 # Displays the faults sorted by major faults along with the process.
 ps -eo min_flt,maj_flt,pcpu,cmd --sort=-maj_flt
 echo"============= checking memory faults - end =================================="
-
-echo"============= checking ping latency - start ================================"
-
-echo"============= checking ping average - end =================================="
-
+yes ''| sed ${gapPrint}q
 echo"============= checking oom score - start ================================"
 sudo ps aux | grep java  &
 CRT_MEM_PNAME_PID=$!
 cat /proc/$CRT_MEM_PNAME_PID/oom_score
 echo"============= checking oom score - end =================================="
-
+yes ''| sed ${gapPrint}q
 echo"============= checking swap memory issues - start ================================"
 # Printing free memory and available memory in megabytes
 free -w -t --mega 
 echo "---------------------------------------"
 vmstat
-cat /proc/$CRT_MEM_PNAME_PID/oom_score
+echo "---------------------------------------"
+sar -S 1 3
 echo"============= checking swap memory issues - end =================================="
+yes ''| sed ${gapPrint}q
+echo"============= checking low level memory metrics - start ================================"
+sar -r ALL 1 25
+echo"============= checking low level memory metrics - end ================================"
+yes ''| sed ${gapPrint}q
+echo"============= checking file handler metrics - start ================================"
+echo ":total allocated - currently used handles - maximum file handles:"
+sysctl fs.file-nr
+echo"============= checking file handler metrics - end ================================"
+yes ''| sed ${gapPrint}q
+echo"============= checking io stats for block devices - start ================================"
+iostat -p | grep -v loop
+echo"============= checking io stats for block devices - end ================================"
+yes ''| sed ${gapPrint}q
+exit 0
+
